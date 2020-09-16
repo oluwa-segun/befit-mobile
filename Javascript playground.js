578d367e2718dc0eafa517e5c56a201fe38c132e
apex apex <mosessamuel589@gmail.com>
2020-09-16 01:27:57 +0100

    commit

---
+++ Javascript playground.js
@@ -0,0 +1,61 @@
+// let arrayExample = [, ];
+// for (let i = 0; i <= 8; i++){
+//     let hash = "#"
+//     arrayExample.push(i)
+//     console.log(arrayExample)
+// }
+
+// var setColor = document.getElementsByTagName('h1');
+
+// console.log(setColor)
+
+// const sam = (height, weight, age)=>{
+//     let power = height * height;
+//      Math.sqrt(power)
+//     let result = (power / weight) + age;
+//     return result
+// }
+
+// console.log(sam(22, 14, 52));
+
+// function randomPointOnCircle(radius) { 
+//     let angle = Math.random() * 2 * Math.PI;
+//     return {x: radius * Math.cos(angle),
+//     y: radius * Math.sin(angle)};
+//     }
+//     console.log(randomPointOnCircle(2)); 
+
+// const btn_blue = document.querySelector('#white');
+
+// btn_blue.addEventListener('click', blueChange);
+
+// function blueChange (e){
+//     document.querySelector(':root').style.backgroundColor = 'rgb(255, 255, 255)'
+//     document.querySelectorAll('body').style.color = 'black'
+// }
+
+
+// const btn_teal = document.querySelector('#teal');
+
+// btn_teal.addEventListener('click', tealChange);
+
+// function tealChange (e){
+//     document.querySelector(':root').style.backgroundColor = 'rgb(1, 80, 80)'
+// }
+
+// const menu = document.querySelector('.nav-menu')
+
+// menu.addEventListener('click', whenClicked)
+
+// function whenClicked (e){
+
+//     if(whenClicked = true){
+//     document.querySelector('.line-1').style.transform = 'translateY(10px) rotate(-45deg)'
+//     document.querySelector('.line-2').style.opacity = '0'
+//     document.querySelector('.line-3').style.transform = 'translateY(-7px) rotate(45deg)'
+//     document.querySelector(':root').style.backgroundColor = 'rgb(65, 64, 64)'
+//     document.querySelector('body').style.opacity = '0.4'
+//     }else{
+        
+//     }
+// }
